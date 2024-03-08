df41528ba2ea9b03a97c7535f8d4d5d203dac318
ThatFrostyy <juniorjegej@gmail.com>
2024-03-08 01:20:52 +0100

    CraftForm changes

--- CraftForm.Designer.cs
+++ CraftForm.Designer.cs
@@ -28,87 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            flowLayoutPanel1 = new FlowLayoutPanel();
             recipeGrid = new DataGridView();
-            flowLayoutPanel2 = new FlowLayoutPanel();
             consoleBox = new TextBox();
             craftButton = new Button();
-            flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)recipeGrid).BeginInit();
-            flowLayoutPanel2.SuspendLayout();
             SuspendLayout();
-            // 
-            // flowLayoutPanel1
-            // 
-            flowLayoutPanel1.Controls.Add(recipeGrid);
-            flowLayoutPanel1.Location = new Point(11, 12);
-            flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(289, 228);
-            flowLayoutPanel1.TabIndex = 0;
             // 
             // recipeGrid
             // 
             recipeGrid.AllowUserToAddRows = false;
             recipeGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            recipeGrid.Location = new Point(3, 3);
+            recipeGrid.Location = new Point(11, 24);
+            recipeGrid.Margin = new Padding(2, 2, 2, 2);
             recipeGrid.Name = "recipeGrid";
             recipeGrid.ReadOnly = true;
             recipeGrid.RowHeadersWidth = 62;
-            recipeGrid.Size = new Size(286, 225);
+            recipeGrid.Size = new Size(278, 134);
             recipeGrid.TabIndex = 0;
-            // 
-            // flowLayoutPanel2
-            // 
-            flowLayoutPanel2.Controls.Add(consoleBox);
-            flowLayoutPanel2.Location = new Point(14, 247);
-            flowLayoutPanel2.Name = "flowLayoutPanel2";
-            flowLayoutPanel2.Size = new Size(561, 150);
-            flowLayoutPanel2.TabIndex = 1;
             // 
             // consoleBox
             // 
             consoleBox.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            consoleBox.Location = new Point(4, 5);
-            consoleBox.Margin = new Padding(4, 5, 4, 5);
+            consoleBox.Location = new Point(12, 163);
             consoleBox.MaxLength = 1000;
             consoleBox.Multiline = true;
             consoleBox.Name = "consoleBox";
             consoleBox.ReadOnly = true;
             consoleBox.ScrollBars = ScrollBars.Vertical;
-            consoleBox.Size = new Size(558, 147);
+            consoleBox.Size = new Size(392, 90);
             consoleBox.TabIndex = 0;
             // 
             // craftButton
             // 
-            craftButton.Location = new Point(367, 105);
+            craftButton.Location = new Point(296, 133);
+            craftButton.Margin = new Padding(2, 2, 2, 2);
             craftButton.Name = "craftButton";
-            craftButton.Size = new Size(154, 34);
+            craftButton.Size = new Size(108, 25);
             craftButton.TabIndex = 2;
             craftButton.Text = "Begin Crafting";
             craftButton.UseVisualStyleBackColor = true;
             // 
             // CraftForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(589, 442);
+            ClientSize = new Size(412, 265);
+            Controls.Add(consoleBox);
+            Controls.Add(recipeGrid);
             Controls.Add(craftButton);
-            Controls.Add(flowLayoutPanel2);
-            Controls.Add(flowLayoutPanel1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "CraftForm";
             Text = "Crafting";
-            flowLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)recipeGrid).EndInit();
-            flowLayoutPanel2.ResumeLayout(false);
-            flowLayoutPanel2.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private FlowLayoutPanel flowLayoutPanel1;
         private DataGridView recipeGrid;
-        private FlowLayoutPanel flowLayoutPanel2;
         private TextBox consoleBox;
         private Button craftButton;
     }
--- Recipes.cs
+++ Recipes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Survival
+﻿namespace Crafting
 {
     public class Recipes
     {
