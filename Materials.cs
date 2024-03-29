68100e158ecab1d9809ad13dd6a4728d0f531206
Olivia <ohinson01@gmail.com>
2024-03-28 20:31:52 -0400

    Updated crafting files

--- Materials.cs
+++ Materials.cs
@@ -5,8 +5,10 @@
         public Materials(int playerLevel, string name, string iconPath, string playerInventory)
             : base(playerLevel, name, iconPath, playerInventory)
         {
+           // TODO: Add getters and setters to drain food/hunger when crafting? 
         }
         
+        // TODO: Add logic to retrieve inventory items 
        
     }
 }
